9fe5d8702499d6a56aab73f9151d4264c52596a6
Danh-Lan <danhlan.nguyen1@gmail.com>
2025-11-07 15:27:43 +0100

    refactor the mask prediction

--- inference.py
+++ inference.py
@@ -6,44 +6,44 @@ import os
 
 from unet import UNet
 
-def single_image_inference(image_pth, model_pth, output_pth, device):
-    model = UNet(in_channels=1, num_classes=2).to(device)
-    model.load_state_dict(torch.load(model_pth, map_location=torch.device(device)))
-
+def image_to_tensor(image_path):
     transform = transforms.Compose([
         transforms.Resize((256, 256)),
         transforms.ToTensor()
     ])
 
-    img = Image.open(image_pth).convert("L")
-    input_img = transform(img).float().to(device)
-    input_img = input_img.unsqueeze(0)
+    image = Image.open(image_path).convert("L")
+    output = transform(image).float()
+    return output
+
+def predict(image_pth, model_pth, output_pth, device):
+    model = UNet(in_channels=1, num_classes=2).to(device)
+    model.load_state_dict(torch.load(model_pth, map_location=torch.device(device)))
+
+    input_img = image_to_tensor(image_pth).unsqueeze(0).to(device)
 
     model.eval()
     with torch.no_grad():
         logits = model(input_img)
-        
         probabilities = torch.softmax(logits, dim=1)
         pred_mask = torch.argmax(probabilities, dim=1)
 
-    pred_mask_np = pred_mask.squeeze(0).cpu().numpy()    
+    pred_mask_np = pred_mask.squeeze(0).cpu().numpy()
     mask_pil = Image.fromarray((pred_mask_np * 255).astype(np.uint8))
 
     # Save results
     os.makedirs(output_pth, exist_ok=True)
-    input_save_path = os.path.join(output_pth, "input_image.png")
-    mask_save_path = os.path.join(output_pth, "predicted_mask.png")
+    input_save_path = os.path.join(output_pth, "input_image.jpg")
+    mask_save_path = os.path.join(output_pth, "predicted_mask.jpg")
 
-    img.save(input_save_path)
     mask_pil.save(mask_save_path)
-
-    print(f"Input image saved to: {input_save_path}")
     print(f"Binary mask saved to: {mask_save_path}")
 
 if __name__ == "__main__":
-    SINGLE_IMG_PATH = "./test/tongue.jpg"
-    MODEL_PATH = "./checkpoints/unet_epoch_10.pth"
+    # SINGLE_IMG_PATH = "./test/tongue.jpg"
+    SINGLE_IMG_PATH = "./data/test/-0011_51_-__jpg.rf.60a43eeb0ea548e7f1cd4431a2f5ee71.jpg"
+    MODEL_PATH = "./checkpoints/unet_best_model.pth"
     OUTPUT_PATH = "./test"
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    single_image_inference(SINGLE_IMG_PATH, MODEL_PATH, OUTPUT_PATH, device)
+    predict(SINGLE_IMG_PATH, MODEL_PATH, OUTPUT_PATH, device)
